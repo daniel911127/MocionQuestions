cf4e1d77a310fdc21c2cc02190ba223b0022209e
Daniel Moreno <daniel_9127@hotmail.com>
2024-02-26 21:34:22 -0500

    added first page

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <div className="frame">
+  <div className="center">
+    <div className="number">
+      <div className="one-one"></div>
+      <div className="one-two"></div>
+      <div className="zero-one"></div>
+    </div>
+    <span className="big">Mocion</span>
+    <span className="small">Challenge</span>
+  </div>
+</div>
+    </div>
+  );
+}
+
+export default App;
